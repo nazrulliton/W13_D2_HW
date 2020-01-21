06f7e9d48c1b19fe22c0d4abe29fc3acf0b8bb57
naz_liton <nazrulliton@icloud.com>
2020-01-21 19:00:08 +0000

    models populated

--- src/main/java/com/codeclan/example/mtrack/models/File.java
+++ src/main/java/com/codeclan/example/mtrack/models/File.java
@@ -1,4 +1,34 @@
 package com.codeclan.example.mtrack.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
 public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "extension")
+    private String extension;
+
+    @Column(name = "size")
+    private int size;
+
+    @JsonIgnoreProperties("filed")
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    private Folder folder;
+   
+
+
+
+
+
 }
