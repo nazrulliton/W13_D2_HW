10a6f2f1478e8771907f4b9dd08f8fc581765fee
naz_liton <nazrulliton@icloud.com>
2020-01-21 18:30:03 +0000

    folder structure created

---
+++ src/main/java/com/codeclan/example/mtrack/MtrackApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.mtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/mtrack/MtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.mtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
