a43f7bb5349e75b617277f32ad41941c944a442f
naz_liton <nazrulliton@icloud.com>
2020-01-21 19:26:20 +0000

    file models updated

--- src/main/java/com/codeclan/example/mtrack/components/DataLoader.java
+++ src/main/java/com/codeclan/example/mtrack/components/DataLoader.java
@@ -1,9 +1,54 @@
 package com.codeclan.example.mtrack.components;
 
 
+import com.codeclan.example.mtrack.models.File;
+import com.codeclan.example.mtrack.models.Folder;
+import com.codeclan.example.mtrack.models.User;
+import com.codeclan.example.mtrack.repositories.FileRepository;
+import com.codeclan.example.mtrack.repositories.FolderRepository;
+import com.codeclan.example.mtrack.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataLoader {
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    FolderRepository folderRepository;
+    @Autowired
+    FileRepository fileRepository;
+
+    public DataLoader(){
+
+    }
+
+    public void run(ApplicationArguments args){
+        User dave = new User("Big Davie");
+        userRepository.save(dave);
+        User john = new User("Big John");
+        userRepository.save(john);
+
+        Folder homework = new Folder("Homework files", dave);
+        folderRepository.save(homework);
+        Folder picture = new Folder("Picture files", dave);
+        folderRepository.save(picture);
+
+        Folder movie = new Folder("movie files", dave);
+        folderRepository.save(movie);
+        Folder music = new Folder("music files", dave);
+        folderRepository.save(music);
+
+        File dayOne = new File("hw from d1", "txt", 5, homework);
+        fileRepository.save(dayOne);
+
+
+    }
+
+
+
 
 }
--- src/main/java/com/codeclan/example/mtrack/models/File.java
+++ src/main/java/com/codeclan/example/mtrack/models/File.java
@@ -26,9 +26,50 @@ public class File {
     @JoinColumn(name = "folder_id", nullable = false)
     private Folder folder;
 
+    public File(String name, String extension, int size, Folder folder) {
+        this.name = name;
+        this.extension = extension;
+        this.size = size;
+        this.folder = folder;
+    }
 
+    public Long getId() {
+        return Id;
+    }
 
+    public void setId(Long id) {
+        Id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
 }
